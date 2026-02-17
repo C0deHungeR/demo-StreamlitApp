a928ca449bd08dd06ecc6783414b65b83d9639a3
Nachiket Tonge <nachiketstonge@gmail.com>
2026-02-17 15:27:10 +0530

    changed

--- app.py
+++ app.py
@@ -1,31 +1,7 @@
-import streamlit as st
+with st.form("login form"):
+    username = st.text_input("username")
+    pasword = st.text_input("password",type="password")
+    submitted = st.form_submit_button("Login")
 
-# Hardcoded credentials (for demo only)
-VALID_USERNAME = "nachiket"
-VALID_PASSWORD = "1234"
-
-st.title("Simple Login App")
-
-# Create session state for login
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-if not st.session_state.logged_in:
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        if username == VALID_USERNAME and password == VALID_PASSWORD:
-            st.session_state.logged_in = True
-            st.session_state.username = username
-            st.success("Login Successful!")
-            st.rerun()
-        else:
-            st.error("Invalid username or password")
-
-else:
-    st.write(f"Hello {st.session_state.username} 👋")
-    
-    if st.button("Logout"):
-        st.session_state.logged_in = False
-        st.rerun()
+    if submitted :
+        st.success(f"Welcome,{username} !")
